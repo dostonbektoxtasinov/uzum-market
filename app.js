c873ae95bcf4df5fb2744b2d7b236452f7162f2e
Dostonbek <dostonbektoxtasinov909@gmail.com>
2023-04-15 04:51:55 +0500

    uzum market

---
+++ app.js
@@ -0,0 +1,83 @@
+function myFlag() {
+    // document.getElementById('flag-box_js_rus').innerHTML = "<img src='./img/bayroqlar/download.jpg' class='bayroq' alt='rus flag'><p>Russion</p>";
+    // document.getElementById('flag-box_js_')
+    document.getElementById('flag-box_js_rus').style.marginRight = '125px';
+    document.getElementById('flag-box_js_rus').style.marginTop = '0px';
+    document.getElementById('flag-box_js_rus').style.display = 'flex';
+    document.getElementById("flag-box_js_rus").style.color = "#909090";
+    document.getElementById('flag-box_js_rus').style.backgroundColor = '#f3deb5';
+    document.getElementById('flag-box_js_rus').style.padding = '5px 25px 5px 8px';
+    document.getElementById('flag-box_js_rus').style.borderBottomLeftRadius = '12px';
+    document.getElementById('flag-box_js_rus').style.borderBottomRightRadius = '12px';
+    document.getElementById('container').style.marginTop = '30px';
+}
+function bottomIcon() {
+
+    // document.getElementById('bottom-icon_span').innerHTML = "Aralash,<br> mahsulot. <br> Kiyimlar. <br> Soyabon va, <br> ko'z oynak. <br> Parfume. <br> Uy rozg'or.";
+    // document.getElementById('bottom-icon_span').style.backgroundColor = '#fff'
+    // document.getElementById('bottom-icon_span').style.width = '100%'
+    // document.getElementById('bottom-icon_span').style.height = '700px'
+
+    // document.getElementById('advertisement-container').style.marginTop = '50em';
+    // document.getElementById('menu-nav').style.display = 'none';
+    // document.getElementById('x').style.display = 'block';
+    // document.getElementById('x').style.float = 'right';
+    // document.getElementById('x').style.margin = '-30px 50px';
+    // document.getElementById('x').style.cursor = 'pointer';
+    // document.getElementById('x').style.background = '#F4F5F5';
+    // document.getElementById('x').style.borderRadius = '100px';
+    // document.getElementById('x').style.padding = '5px';
+    // document.getElementById('x').style.width = '40px';
+    // document.getElementById('x').style.height = '40px';
+//     document.getElementById('search-nav_box_img').style.display = 'none';    
+//     document.getElementById('search-nav_box_x').style.display = 'block';
+// }
+
+// function x() {
+//     // document.getElementById('bottom-icon_span').style.backgroundColor = 'none'
+//     // document.getElementById('bottom-icon_span').style.width = '100%'
+//     // document.getElementById('bottom-icon_span').style.height = '7px'
+//     // document.getElementById('advertisement-container').style.marginTop = '50em';
+//     document.getElementById('x').style.display = 'none';
+//     document.getElementById('menu-nav').style.display = 'block';
+//     // document.getElementById('x').style.float = 'right';
+//     // document.getElementById('x').style.margin = '-30px 50px';
+//     document.getElementById('x').style.cursor = 'pointer';
+//     // document.getElementById('x').style.background = '#F4F5F5';
+//     // document.getElementById('x').style.borderRadius = '100px';
+//     // document.getElementById('x').style.padding = '5px';
+//     // document.getElementById('x').style.width = '40px';
+//     // document.getElementById('x').style.height = '40px';
+//     document.getElementById('search-nav_box_x').style.display = 'none';
+//     document.getElementById('search-nav_box_img').style.display = 'block';
+// }
+
+function advertisementRight() {
+    document.getElementById('advertisementFirst').style.display = 'none';
+    document.getElementById('advertisementSecond').style.display = 'block';
+}
+function advertisementLeft() {
+    document.getElementById('advertisementFirst').style.display = 'block';
+    document.getElementById('advertisementSecond').style.display = 'none';
+}
+// function advertisementRight() {
+//     document.getElementById
+// }
+
+// function containerItemLove() {
+//     document.getElementById('container-item_love').style.display = 'none';
+//     document.getElementById('container-item_red-love').style.display = 'block';
+}
+
+/* X-onclick */
+
+function bottomIconX() {
+    document.getElementById('yana2-span').style.display = 'none';
+    document.getElementById('yana2').style.display = 'block';
+}
+function bottomIconY() {
+    document.getElementById('yana3-span').style.display = 'none';
+    document.getElementById('yana3').style.display = 'block';
+}
+
+
